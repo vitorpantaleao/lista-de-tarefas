f37e41753b50317a7d14efef4460c1e9d811b2f5
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-04-21 13:26:07 -0300

    configura vue-router

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const Home = import ('../views/Home.vue')
+const Editar = import ('../views/Editar.vue')
+
+const routes = [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/editar', name: 'Editar', component: Editar },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
---
+++ src/views/Editar.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Editar</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
