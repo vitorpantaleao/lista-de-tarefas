0db778dc7033b819a700890d060cb55c6d81aea5
Vitor Pantaleão <vitor-hmp@hotmail.com>
2023-04-21 13:57:09 -0300

    adiciona estrutura para criar uma tarefa e método para salvar no localStorage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router/index'
+import './assets/style.css'
 
 createApp(App)
     .use(router)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,69 @@
 <template>
-    <div>
-        <h1>Home</h1>
-    </div>
+    <main class="app">
+        <section class="greeting">
+            <h2 class="title">
+                Olá, <input type="text" placeholder="Digite seu nome aqui" v-model="name" />
+            </h2>
+        </section>
+
+        <section class="create-todo">
+            <h3>CRIAR TAREFA</h3>
+
+            <form @submit.prevent="addTodo">
+                <h4>Nome da tarefa:</h4>
+                <input type="text" placeholder="ex: criar um App de Tarefas em VueJS" v-model="input_content" />
+                <h4>Descrição:</h4>
+                <input type="text" placeholder="ex: Criar um app de tarefas usando o VueJS 3 e Pinia, usando boas ..." v-model="input_description" />
+                <h4>Escolha uma categoria</h4>
+                <div class="options">
+                    <label>
+                        <input type="radio" name="category" value="business" v-model="input_category" />
+                        <span class="bubble business"></span>
+                        <div>Trabalho</div>
+                    </label>
+                    <label>
+                        <input type="radio" name="category" value="personal" v-model="input_category" />
+                        <span class="bubble personal"></span>
+                        <div>Pessoal</div>
+                    </label>
+                    <label>
+                        <input type="radio" name="category" value="other" v-model="input_category" />
+                        <span class="bubble other"></span>
+                        <div>Outros</div>
+                    </label>
+                </div>
+                <input type="submit" value="Criar Tarefa" />
+            </form>
+        </section>
+    </main>
 </template>
 
 <script setup>
-    
+    import { ref, onMounted, computed, watch } from 'vue'
+
+    const todos = ref([])
+    const name = ref('')
+
+    const input_content = ref('')
+    const input_description = ref('')
+    const input_category = ref(null)
+
+    // adicionando um todo
+    const addTodo = () => {
+        if (input_content.value.trim() === '' || input_description.value.trim() === '' || input_category.value === null) {
+            return
+        }
+        
+        todos.value.push({
+            content: input_content.value,
+            description: input_description.value,
+            category: input_category.value,
+            done: false,
+            created_at: new Date().getTime()
+        })
+
+        input_content.value = ''
+        input_description.value = ''
+        input_category.value = null
+    }
 </script>
